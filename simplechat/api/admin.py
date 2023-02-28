0f9827ac5d48b76f54fd484648ff0652b30ce20b
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-02-28 19:15:35 +0200

    add ThreadModelAdmin

--- simplechat/api/admin.py
+++ simplechat/api/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django import forms
+
 from .models import Thread, Message
 
+admin.site.register(Message)
 
-admin.site.register([Thread, Message])
+
+class ThreadForm(forms.ModelForm):
+    model = Thread
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get('participants').count() != 2:
+            raise ValidationError('Thread must have exactly two participants')
+        thread = self.model.objects.filter(participants=cleaned_data['participants'][0])
+        thread = thread.filter(participants=cleaned_data['participants'][1])
+        if thread.exists():
+            raise ValidationError('Thread already exists')
+
+
+@admin.register(Thread)
+class QuestionAdmin(admin.ModelAdmin):
+    form = ThreadForm
