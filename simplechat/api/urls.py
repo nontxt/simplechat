a511b5e3719f7ba66ab81bd37e29cda932769397
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-02-28 20:07:04 +0200

    add main api endoints

--- simplechat/api/urls.py
+++ simplechat/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ThreadViewSet, UnreadMessageListAPIView
-
+from .views import ThreadModelViewSet, UnreadMessageListCreateAPIView, MessageListCreateAPIView
 
 urlpatterns = [
-    path('threads', ThreadViewSet.as_view({'get': 'list'}), name='thread-list'),
-    path('threads/<str:username>', ThreadViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='thread'),
-    path('unread', UnreadMessageListAPIView.as_view(), name='unread'),
+    path('threads', ThreadModelViewSet.as_view({'get': 'list'}), name='thread-list'),
+    path('threads/<str:username>', ThreadModelViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='thread'),
+    path('messages/unread', UnreadMessageListCreateAPIView.as_view(), name='unread'),
+    path('messages/<str:username>', MessageListCreateAPIView.as_view()),
+
 ]
