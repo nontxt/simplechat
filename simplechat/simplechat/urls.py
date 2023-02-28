541a7adae1bb8956a67d79b172f83416a698cd31
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-02-28 20:01:28 +0200

    add base api and token obtain endpoint

--- simplechat/simplechat/urls.py
+++ simplechat/simplechat/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('token/', obtain_auth_token, name='obtain_auth_token'),
+    path('token/jwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
