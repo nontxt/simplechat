8d417c51b2f7ae392eb7752994dfc4db36b2b74b
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-02-28 20:06:50 +0200

    add main features

--- simplechat/api/views.py
+++ simplechat/api/views.py
@@ -1,26 +1,114 @@
 from rest_framework.response import Response
-from rest_framework.generics import RetrieveAPIView, ListAPIView
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import NotFound
 from rest_framework import status
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from .models import Thread, Message
 from .serializers import ThreadSerializer, MessageSerializer, UnreadMessageSerializer
+from .pagination import CustomPagination
 
 
-class UnreadMessageListAPIView(ListAPIView):
+def is_not_current_user(request, **kwargs):
+    """Return User if exists and is not current"""
+    username = kwargs.get('username', NotFound)
+    if request.user.username == username:
+        raise NotFound
+    return get_object_or_404(User, username=username)
+
+
+class MessageListCreateAPIView(ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    pagination_class = CustomPagination
+    thread = None
+
+    def get_thread(self, interlocutor=None):
+        """ Return a thread for current user and interlocutor"""
+        user_threads = self.request.user.threads.all()
+        thread_id_list = user_threads.values_list('id')
+
+        self.thread = get_object_or_404(Thread, Q(id__in=thread_id_list) & Q(participants=interlocutor))
+        return self.thread
+
+    def get_queryset(self):
+        """ Return a list of messages for current user and interlocutor"""
+        queryset = self.queryset.filter(thread=self.thread)
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        interlocutor = is_not_current_user(self.request, **kwargs)
+        thread = self.get_thread(interlocutor=interlocutor)
+        data = request.data.copy()
+        data['thread'] = thread.id
+        data['sender'] = self.request.user.id
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        thread.update()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def list(self, request, *args, **kwargs):
+        interlocutor = is_not_current_user(self.request, **kwargs)
+        self.get_thread(interlocutor=interlocutor)
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class UnreadMessageListCreateAPIView(ListCreateAPIView):
     queryset = Message.objects.filter(is_read=False)
     serializer_class = UnreadMessageSerializer
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         queryset = self.queryset.filter(thread__participants=self.request.user).exclude(sender=self.request.user)
         return queryset
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
 
-class ThreadViewSet(ModelViewSet):
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'count': queryset.count(), 'messages': serializer.data})
+
+    def create(self, request, *args, **kwargs):
+        match request.data.get('message_id', None):
+            case str() as messages_id:
+                if not messages_id.isdigit():
+                    return Response({'message_id': 'Must be single or list of digit'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                messages_id = [messages_id]
+            case list() as messages_id:
+                ...
+            case _:
+                return Response({'message_id': 'Is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        messages = self.get_queryset().filter(id__in=messages_id)
+        if not messages.exists():
+            raise NotFound
+        for message in messages:
+            message.mark_as_read()
+        return Response(status=status.HTTP_200_OK)
+
+
+class ThreadModelViewSet(ModelViewSet):
     queryset = Thread.objects.all()
     serializer_class = ThreadSerializer
+    pagination_class = CustomPagination
 
     def get_queryset(self, interlocutor=None):
         queryset = self.queryset.filter(participants=self.request.user)
@@ -29,12 +117,9 @@ class ThreadViewSet(ModelViewSet):
         return queryset
 
     def retrieve(self, request, *args, **kwargs):
-        username = kwargs.get('username', NotFound)
-        if self.request.user.username == username:
-            raise NotFound
-        interlocutor = get_object_or_404(User, username=username)
+        interlocutor = is_not_current_user(self.request, **kwargs)
         queryset = self.filter_queryset(self.get_queryset(interlocutor))
-        if queryset.count() == 0:
+        if not queryset.exists():
             thread = Thread.objects.create()
             thread.participants.set([self.request.user, interlocutor])
         else:
@@ -44,8 +129,8 @@ class ThreadViewSet(ModelViewSet):
         return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
-        username = kwargs.get('username', NotFound)
+        interlocutor = is_not_current_user(self.request, **kwargs)
         queryset = self.filter_queryset(self.get_queryset())
-        instance = get_object_or_404(queryset, participants__username=username)
+        instance = get_object_or_404(queryset, participants=interlocutor)
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
