20088539f59efc9bca1177a898646dae0c997822
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-03-01 14:48:16 +0200

    add namespace for message endpoint

--- simplechat/api/urls.py
+++ simplechat/api/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('threads', ThreadModelViewSet.as_view({'get': 'list'}), name='thread-list'),
     path('threads/<str:username>', ThreadModelViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='thread'),
     path('messages/unread', UnreadMessageListCreateAPIView.as_view(), name='unread'),
-    path('messages/<str:username>', MessageListCreateAPIView.as_view()),
-
+    path('messages/<str:username>', MessageListCreateAPIView.as_view(), name='messages'),
 ]
